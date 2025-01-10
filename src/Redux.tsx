ad8979b9344204c6346079d7caa8eb9a97a2fe17
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-09 20:01:40 -0800

    first commit

---
+++ src/Redux.tsx
@@ -0,0 +1,10 @@
+
+const Redux = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Redux;
---
+++ src/main.tsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Root';
+import Home from './Home';
+import Redux from './Redux';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/redux",
+        element: <Redux></Redux>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
